664d1c2f37a77b0fcc9ce6529e5dd2f24aa006ce
Rohan <andriamihaja2007rohan@gmail.com>
2026-07-20 11:29:17 +0300

    Update DashboardController.php

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -12,65 +12,15 @@ class DashboardController extends BaseController
         return view('dashboard');
     }
 
-    public function calculGainParOperateur(int $idOperateur, $dateMin = null, $dateMax = null): array
-    {
-        $typeOperationModel = new TypeOperation();
-        $typeOperations = $typeOperationModel->findAll();
-
-        $answer = [];
-
-        foreach ($typeOperations as $typeOperation) {
-            $answer[$typeOperation['libelle']] = $this->calculGainParTypeOperation(
-                $idOperateur,
-                $typeOperation['id'],
-                $dateMin,
-                $dateMax
-            );
-        }
-
-        return $answer;
-    }
-    public function calculGain(int $idTypeOperation, int $idOperateur, $dateMin = null, $dateMax = null)
+    // Controller
+    public function afficherGainParOperateur(int $idOperateur, $dateMin = null, $dateMax = null)
     {
         $mouvementModel = new Mouvement();
+        $answer = $mouvementModel->calculGainParOperateur($idOperateur, $dateMin, $dateMax);
+        $answer['liste'] = $mouvementModel->getMouvementDetails($idOperateur, $dateMin, $dateMax);
+        // $answer = ['Depot' => 0, 'Retrait' => 50, 'Transfert' => 200]
+        $answer['total'] = array_sum($answer);
 
-        $builder = $mouvementModel->builder();
-
-        $builder->selectSum('montantFrais', 'frais')
-            ->where('idTypeOperation', $idTypeOperation)
-            ->where('idOperateur', $idOperateur);
-
-        if ($dateMin !== null) {
-            $builder->where('dateMouvement >=', $dateMin);
-        }
-
-        if ($dateMax !== null) {
-            $builder->where('dateMouvement <=', $dateMax);
-        }
-
-        return $builder->get()->getRowArray()['frais'];
-    }
-
-    public function calculGainParTypeOperation(int $idOperateur, int $idTypeOperation, $dateMin = null, $dateMax = null)
-    {
-        $mouvementModel = new Mouvement();
-
-        $builder = $mouvementModel->builder();
-
-        $builder->selectSum('montantFrais', 'frais')
-            ->where('idOperateur', $idOperateur)
-            ->where('idTypeOperation', $idTypeOperation);
-
-        if ($dateMin !== null) {
-            $builder->where('dateMouvement >=', $dateMin);
-        }
-
-        if ($dateMax !== null) {
-            $builder->where('dateMouvement <=', $dateMax);
-        }
-
-        $result = $builder->get()->getRowArray();
-
-        return $result['frais'] ?? 0;
+        return view('operator/operator', $answer);
     }
 }
