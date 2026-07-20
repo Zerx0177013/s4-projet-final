84dda36e8efcde685724b8595f8f48e6e3076185
Rohan <andriamihaja2007rohan@gmail.com>
2026-07-20 11:28:25 +0300

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,4 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/operator', 'CompteController::afficherComptes');
+$routes->get('rapport/gain/(:num)', 'DashboardController::afficherGainParOperateur/$1');
